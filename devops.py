9a4eb5c370eeabcbd3310bc4542fd085e61f77cb
Sireeshagajula <145096319+Sireeshagajula@users.noreply.github.com>
2023-12-09 09:41:20 +0530

    Add files via upload

--- devops.py
+++ devops.py
@@ -1,23 +1,9 @@
-print("Armstrong Numbers Between 1 and 1000:")
-first = 1
-last = 1000
-while first<=last:
-  res = 0
-  temp = first
-  noOfDigit = 0
-  while temp>0:
-    temp = int(temp/10)
-    noOfDigit = noOfDigit + 1
-  num = first
-  while num>0:
-    rem = num%10
-    pow = 1
-    i = 0
-    while i<noOfDigit:
-      pow = pow*rem
-      i = i+1
-    res = res+pow
-    num = int(num/10)
-  if res == first:
-    print(res)
-  first = first+1
+for i in range(1,999):
+	a=i
+	sum=0
+	while(i>0):
+		rem=i%10
+		sum=sum+(rem*rem*rem)
+		i=i//10
+	if(sum==a):
+		print(a, "is a Armstrong Number")
